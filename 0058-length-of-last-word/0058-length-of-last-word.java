1ea3b663e58278fc55dd343148bdebcf2b16acf9
ambia <ambiafatima7@gmail.com>
2026-08-28 20:02:08 +0530

    Time: 0 ms (100%), Space: 43 MB (51.55%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+         int i = s.length() - 1;
+
+        // Skip spaces at the end
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count the last word
+        int length = 0;
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
